e37f2b32a673b5bc0d853585aae5662b839e8abf
Sullivan Beck <sbeck@cpan.org>
2010-02-25 12:58:08 +0000

    Import of SBECK/Set-ArrayAlt-1.02 from CPAN.
    gitpan-cpan-distribution: Set-ArrayAlt
    gitpan-cpan-version:      1.02
    gitpan-cpan-path:         SBECK/Set-ArrayAlt-1.02.tar.gz
    gitpan-cpan-author:       SBECK
    gitpan-cpan-maturity:     released

--- lib/Set/ArrayAlt.pm
+++ lib/Set/ArrayAlt.pm
@@ -12,7 +12,7 @@ use strict;
 use Sort::DataTypes qw(sort_by_method sort_valid_method);
 
 use vars qw($VERSION);
-$VERSION = "1.01";
+$VERSION = "1.02";
 
 ###############################################################################
 # BASE METHODS
